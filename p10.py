bfad7a3d8b8c694547ae90739157556b29b49b6e
manjupinky <41039746+manjupinky@users.noreply.github.com>
2018-09-17 12:38:55 +0530

    Add files via upload

--- p10.py
+++ p10.py
@@ -1,7 +1,7 @@
 Num = int(input("Please Enter any Number: "))
 Count = 0
 while(Num > 0):
-    Num = Num// 10
+    Num = Num // 10
     Count = Count + 1
  
 print("\n Number of Digits in a Given Number = %d" %Count)
