9113292c2843d3c2d0093c82200fa1af6df15aad
manjupinky <41039746+manjupinky@users.noreply.github.com>
2018-12-27 15:23:20 +0530

    Add files via upload

---
+++ p36.py
@@ -0,0 +1,13 @@
+ 
+string = input("Please Enter your Own String : ")
+ special = 0
+
+for i in range(len(string)):
+    if(string[i].isspecial()):
+        
+    special = special + 1
+    
+        
+
+print("Total Number of Special Characters in this String :  ", special)
+
