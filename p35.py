6715519941bf0e68c7e5bffeec003762a39cf2e5
manjupinky <41039746+manjupinky@users.noreply.github.com>
2018-12-27 15:07:10 +0530

    Update p35.py

--- p35.py
+++ p35.py
@@ -1,8 +1,8 @@
 string=raw_input("Enter string:")
 count1=0
 count2=0
-for i in string:
-      if(i.isdigit()):
+for x in string:
+      if(x.isdigit()):
             count1=count1+1
       count2=count2+1
 print("The number of digits is:")
