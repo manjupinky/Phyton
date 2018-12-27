0f9f680cc8cb2a35813df000680b5cb8dd51c720
manjupinky <41039746+manjupinky@users.noreply.github.com>
2018-12-27 15:26:32 +0530

    Update p37.py

--- p37.py
+++ p37.py
@@ -1,8 +1,10 @@
 print("Enter 'x' for exit.");
 num1 = input("Enter first number: ");
+
 if num1 == 'x':
     exit();
-else:
+
+    else:
     num2 = input("Enter second number: ");
     print("\nStarted swapping the given two numbers...");
     number1 = int(num1);
