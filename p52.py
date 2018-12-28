8fec903f21c911cace24f21654e0b74eba20166d
manjupinky <41039746+manjupinky@users.noreply.github.com>
2018-12-28 11:39:31 +0530

    Update p52.py

--- p52.py
+++ p52.py
@@ -1,9 +1,9 @@
 string=raw_input("Enter string:")
 char=0
 word=1
-for i in string:
+for s in string:
       char=char+1
-      if(i==' '):
+      if(s==' '):
             word=word+1
 print("Number of words in the string:")
 print(word)
