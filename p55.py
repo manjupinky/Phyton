7cf9b21bcea9af0b1a6b29d7c69af2c23c31113d
manjupinky <41039746+manjupinky@users.noreply.github.com>
2018-12-28 11:43:23 +0530

    Update p55.py

--- p55.py
+++ p55.py
@@ -2,7 +2,7 @@ a=int(input("Enter first number: "))
 b=int(input("Enter second number: "))
 print("Product is: ",product(a,b))
 product = int(input("Enter a number: "))
-if (num % 2) == 0:
-   print("{0} is Even".format(num))
+if (product% 2) == 0:
+   print("{0} is Even".format(product))
 else:
-   print("{0} is Odd".format(num))
+   print("{0} is Odd".format(product))
