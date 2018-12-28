9da071fe124dec3c2bdaf032e1be823dec625156
manjupinky <41039746+manjupinky@users.noreply.github.com>
2018-12-28 15:36:08 +0530

    Update p83.py

--- p83.py
+++ p83.py
@@ -1,13 +1,13 @@
-ip=input("Enter your seqence (Mod/Dividee):")
+g=input("Enter your seqence (Mod/Dividee):")
 op=['%','/']
-for x1 in ip:
+for x1 in g:
 	if x1 in op:
 		if(x1=='%'):
-			k1=int(ip.split(x1)[0])
-			k2=int(ip.split(x1)[1])
+			k1=int(g.split(x1)[0])
+			k2=int(g.split(x1)[1])
 			ans=k1%k2
 		elif(x1=='/'):
-			k1=int(ip.split(x1)[0])
-			k2=int(ip.split(x1)[1])
+			k1=int(g.split(x1)[0])
+			k2=int(g.split(x1)[1])
 			ans=k1//k2
 print(ans)
