07244429fe918882add7ca1a5b0c2821ada1e2f7
manjupinky <41039746+manjupinky@users.noreply.github.com>
2018-12-28 11:37:18 +0530

    Add files via upload

---
+++ p55.py
@@ -0,0 +1,8 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+print("Product is: ",product(a,b))
+product = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
