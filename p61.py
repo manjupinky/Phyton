cac40712e8d2a974ca5ecbc4978792bc7477c566
manjupinky <41039746+manjupinky@users.noreply.github.com>
2018-12-28 13:58:11 +0530

    Update p61.py

--- p61.py
+++ p61.py
@@ -1,3 +1,11 @@
-string=raw_input("enter the string\n")
-k=int(input())
-print(string[:k])
+def firstk():
+	str=input()
+	s=''
+	k=int(input())
+	for i in range(0,k):
+		s+=str[i]
+	print(s)
+try:
+	firstk()
+except:
+	print('invalid')
