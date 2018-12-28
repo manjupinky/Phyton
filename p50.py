ccbfdb4f8eab2f65a989ed27c9cf3f197b86afd5
manjupinky <41039746+manjupinky@users.noreply.github.com>
2018-12-28 11:01:38 +0530

    Add files via upload

---
+++ p50.py
@@ -0,0 +1,22 @@
+# Function to check if x is power of 2 
+def isPowerOfTwo(n): 
+    if (n == 0): 
+        return False
+    while (n != 1): 
+            if (n % 2 != 0): 
+                return False
+            n = n // 2
+              
+    return True
+  
+# Driver code 
+if(isPowerOfTwo(31)): 
+    print('Yes') 
+else: 
+    print('No') 
+if(isPowerOfTwo(64)): 
+    print('Yes') 
+else: 
+    print('No') 
+  
+# This code is contributed by Danish Raza 
