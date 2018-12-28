2b309760fca82f4c25848b9c37e64963e97284ac
manjupinky <41039746+manjupinky@users.noreply.github.com>
2018-12-28 11:11:35 +0530

    Update p49.py

--- p49.py
+++ p49.py
@@ -1,9 +1,9 @@
 val1 = float(input("Enter first number: "))
 val2 = float(input("Enter second number: "))
 
-# Adding the numbers
+
 sum = val1 + val2
 
-# Displaying the result
+
 print("The sum of input numbers is: ", sum)
 
