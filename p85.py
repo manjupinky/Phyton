7ff246c138220dc292cd7eaa27036e8bb7b049fa
manjupinky <41039746+manjupinky@users.noreply.github.com>
2019-01-08 14:34:45 +0530

    Add files via upload

---
+++ p85.py
@@ -0,0 +1,14 @@
+def str1to2():
+	s=input()
+	l=list(s)
+	(e,o)=('','')
+	for i in range(len(l)):
+		if i%2==0:
+			e+=l[i]
+		else :
+			o+=l[i]
+	print(e,o);
+try:
+	str1to2()
+except:
+	print('invalid');
