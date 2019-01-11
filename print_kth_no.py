eca85ba229fd8961aed0fcc5979881e1ad85e21f
manjupinky <41039746+manjupinky@users.noreply.github.com>
2019-01-11 13:52:23 +0530

    Update print_kth_no.py

--- print_kth_no.py
+++ print_kth_no.py
@@ -1,8 +1,8 @@
 n1=int(input("Enter N value:"))
 k1=int(input("Enter K value:"))
 print("Enter the array values:")
-a=[]
+h=[]
 for x in range(0,n1):
 	t=int(input())
-	a.append(t)
+	h.append(t)
 print(a[k1-1])
